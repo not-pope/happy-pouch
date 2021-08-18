75e0f006357a9e666e18baf8f92cae42b26d53f4
not-pope <72689979+not-pope@users.noreply.github.com>
2021-08-18 22:55:47 +0000

    finally some progress

---
+++ script.js
@@ -0,0 +1,11 @@
+function startTimer(){
+  let setup=document.getElementById("setup");
+  let timer=document.getElementById("timer");
+  // let minutes=document.getElementById("timer-minutes")
+  let buttom=document.getElementById("button");
+  // console.log(setup);
+  setup.style.display="none";
+  
+  timer.style.display="inline";
+  button.innerHTML="Pause";
+}
